c35b51f3ad8a84d884fd60422154288b3993cb78
Ross Sharrott <rsharrott@gmail.com>
2010-05-28 12:32:08 +1000

    CardView stub to fix broken build

---
+++ jFlash/Classes/CardView.h
@@ -0,0 +1,16 @@
+//
+//  CardView.h
+//  jFlash
+//
+//  Created by シャロット ロス on 5/25/10.
+//  Copyright 2010 LONG WEEKEND INC.. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CardView : UIViewController {
+
+}
+
+@end
