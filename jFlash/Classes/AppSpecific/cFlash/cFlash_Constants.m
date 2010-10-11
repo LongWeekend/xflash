73bfd6b20facdb6f37f5bd7f185a5d592ac7547a
Mark Makdad <makdad@gmail.com>
2010-10-11 14:17:18 +0200

    Added new files for C-Flash - constants, etc

---
+++ jFlash/Classes/AppSpecific/cFlash/cFlash_Constants.m
@@ -0,0 +1,7 @@
+//
+//  cFlash_Constants.m
+//  jFlash
+//
+//  Created by Mark Makdad on 10/11/10.
+//  Copyright 2010 Long Weekend Inc. All rights reserved.
+//
---
+++ jFlash/Classes/AppSpecific/jFlash/jFlash_Constants.m
@@ -0,0 +1,25 @@
+//
+//  jFlash_Constants.m
+//  jFlash
+//
+//  Created by Mark Makdad on 10/11/10.
+//  Copyright 2010 Long Weekend Inc. All rights reserved.
+//
+
+// Version numbers & migration constants - DO NOT CHANGE unless you know what you're doing
+NSString * const JFLASH_CURRENT_VERSION       = @"1.3";
+NSString * const JFLASH_CURRENT_CARD_DATABASE = @"jFlash-CARD-1.1.db";
+NSString * const JFLASH_CURRENT_USER_DATABASE = @"jFlash.db";
+
+NSString * const JFLASH_VERSION_1_0           = @"1.0";
+NSString * const JFLASH_VERSION_1_1           = @"1.1";
+NSString * const JFLASH_VERSION_1_2           = @"1.2";
+NSString * const JFLASH_VERSION_1_3           = @"1.3";
+
+NSString * const JFLASH_10_USER_DATABASE      = @"jFlash.db";
+NSString * const JFLASH_10_TO_11_SQL_FILENAME = @"jflash_10_to_11.sql";
+
+// Plugin keys - DO NOT change
+NSString *const CARD_DB_KEY = @"CARD_DB";
+NSString *const FTS_DB_KEY = @"FTS_DB";
+NSString *const EXAMPLE_DB_KEY = @"EX_DB";
