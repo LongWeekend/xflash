b423a1be20dea23f7d08acacd4ac9d8b64bb0aa7
Mark Makdad <makdad@gmail.com>
2011-05-24 12:41:15 +0900

    Getting rid of the separate target constants file approach

--- jFlash/Classes/AppSpecific/cFlash/cFlash_Constants.h
+++
@@ -1,9 +0,0 @@
-//
-//  cFlash_Constants.h
-//  jFlash
-//
-//  Created by Mark Makdad on 10/11/10.
-//  Copyright 2010 Long Weekend Inc. All rights reserved.
-//
-
-extern NSString * const LWE_CF_VERSION_1_0;
