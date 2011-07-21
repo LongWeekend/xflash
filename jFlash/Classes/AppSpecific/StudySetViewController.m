b7151ca6103acd1b06d2d9645533e5f2cb5a4df8
rpranata <rendy.pranata@gmail.com>
2011-07-21 13:57:04 +1000

    Removed the NSLog.

--- jFlash/Classes/AppSpecific/StudySetViewController.m
+++ jFlash/Classes/AppSpecific/StudySetViewController.m
@@ -542,7 +542,6 @@ enum Sections {
   {
     CurrentState *state = [CurrentState sharedCurrentState];
     Tag* tmpTag = [tagArray objectAtIndex:indexPath.row];
-    NSLog(@"Tag : %@", tmpTag);
     if ([tmpTag tagEditable] && ([[state activeTag] tagId] != [tmpTag tagId]))
     {
       return YES;
