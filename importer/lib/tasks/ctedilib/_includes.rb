e2c475876d22dc0b771b4b5915ab44732c463f07
Mark Makdad <makdad@gmail.com>
2011-07-29 23:14:35 +0900

    Removed reference to old _importer.rb filename

--- importer/lib/tasks/ctedilib/_includes.rb
+++ importer/lib/tasks/ctedilib/_includes.rb
@@ -20,17 +20,9 @@ load File.dirname(__FILE__) + "/classes/CSVParser.rb"
 load File.dirname(__FILE__) + "/classes/BookListParser.rb"
 
 # Importers
-load File.dirname(__FILE__) + "/classes/_importer.rb"
+load File.dirname(__FILE__) + "/classes/CEdictBaseImporter.rb"
 load File.dirname(__FILE__) + "/classes/CEdictImporter.rb"
-
 load File.dirname(__FILE__) + "/classes/CEdictExporter.rb"
-#load File.dirname(__FILE__) + "/classes/_jflash_importer.rb"
-#load File.dirname(__FILE__) + "/classes/_jflash_migration.rb"
-#load File.dirname(__FILE__) + "/classes/_npedia_importer.rb"
-#load File.dirname(__FILE__) + "/classes/_jmdict_parser.rb"
-#load File.dirname(__FILE__) + "/classes/_tanc_parser.rb"
-#load File.dirname(__FILE__) + "/classes/_tanc_2_jflash_importer.rb"
-#load File.dirname(__FILE__) + "/classes/_kanjidic_2_jflash_importer.rb"
 
 # Required Gems
 require 'levenshtein'
