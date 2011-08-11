99a624cac283f00f1289adbc26821dd8eef0cfca
rpranata <rendy.pranata@gmail.com>
2011-08-12 04:32:23 +1000

    Added both small dataset and new testing method for matching the card with some tags entry. (Testing purposes)

--- importer/lib/tasks/ctedilib/tests/TagParserTest.rb
+++ importer/lib/tasks/ctedilib/tests/TagParserTest.rb
@@ -30,4 +30,16 @@ class TagParserTest < Test::Unit::TestCase
     importer.import()
   end
   
+  def test_800_fail
+    test_file_path = File.dirname(__FILE__) + "/../../../../data/cedict/tags/test_800+800020100915-rare_cases.csv"
+    parser = CSVParser.new(test_file_path)
+    results = parser.run()
+    
+    test_configuration_file = File.dirname(__FILE__) + "/../../../../config/tags_config/file_800_config.yml"
+    configuration = TagsBaseConfiguration.new(test_configuration_file)
+    
+    importer = Tags800WordsImporter.new(results)
+    importer.import()
+  end
+  
 end
