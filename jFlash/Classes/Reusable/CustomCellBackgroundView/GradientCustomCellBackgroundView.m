dfd77c66689a46cbf7cc0e83a8fccca6a106cc92
Ross Sharrott <rsharrott@gmail.com>
2011-08-15 13:57:45 -0400

    removed commented code

--- jFlash/Classes/Reusable/CustomCellBackgroundView/GradientCustomCellBackgroundView.m
+++ jFlash/Classes/Reusable/CustomCellBackgroundView/GradientCustomCellBackgroundView.m
@@ -4,8 +4,6 @@
 
 #import "GradientCustomCellBackgroundView.h"
 
-static void addRoundedRectToPath(CGContextRef context, CGRect rect,float ovalWidth,float ovalHeight);
-
 @implementation GradientCustomCellBackgroundView
 @synthesize position;
 
@@ -169,26 +167,3 @@ static void addRoundedRectToPath(CGContextRef context, CGRect rect,float ovalWid
 
 @end
 
-
-//static void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth,float ovalHeight)
-//{
-//  float fw, fh;
-//  if (ovalWidth == 0 || ovalHeight == 0) {// 1
-//    CGContextAddRect(context, rect);
-//    return;
-//  }
-//  
-//  CGContextSaveGState(context);// 2
-//  CGContextTranslateCTM (context, CGRectGetMinX(rect),// 3
-//                         CGRectGetMinY(rect));
-//  CGContextScaleCTM (context, ovalWidth, ovalHeight);// 4
-//  fw = CGRectGetWidth (rect) / ovalWidth;// 5
-//  fh = CGRectGetHeight (rect) / ovalHeight;// 6
-//  CGContextMoveToPoint(context, fw, fh/2); // 7
-//  CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);// 8
-//  CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);// 9
-//  CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);// 10
-//  CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1); // 11
-//  CGContextClosePath(context);// 12
-//  CGContextRestoreGState(context);// 13
-//}
