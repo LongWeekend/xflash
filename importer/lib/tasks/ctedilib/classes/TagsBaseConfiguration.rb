bb77c696bca3d37d9b8681b937b6f8429d5407a5
rpranata <rendy.pranata@gmail.com>
2011-08-19 19:40:01 +1000

    Made the yml file to put a metadata of a source tag-file and get the information from there.

--- importer/lib/tasks/ctedilib/classes/TagsBaseConfiguration.rb
+++ importer/lib/tasks/ctedilib/classes/TagsBaseConfiguration.rb
@@ -3,12 +3,18 @@ require 'YAML'
 class TagsBaseConfiguration
   
   ### Class Constructor
-  def initialize (file_to_yaml_file)
+  def initialize (file_to_yaml_file, metadata_key="")
     @configuration = YAML.load_file(file_to_yaml_file)
+    @configuration = @configuration[metadata_key] unless ((metadata_key == nil)||(metadata_key.strip().length()<=0))
     
-    #p 'Initialised'
-    p @configuration
+    if (@configuration == nil)
+      raise "Sorry I have to throw an exception as the metadata key (second parameter) supplied is not found in the file"
+    end
     return self
   end
   
+  def file_name
+    return @configuration["file_name"]
+  end
+
 end
