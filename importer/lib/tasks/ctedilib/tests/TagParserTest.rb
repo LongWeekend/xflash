f5044300941e68b4330ffdd27b3b738247c2505e
rpranata <rendy.pranata@gmail.com>
2011-08-22 12:37:52 +1000

    Fixed the test method for importing the 800+ advance file

--- importer/lib/tasks/ctedilib/tests/TagParserTest.rb
+++ importer/lib/tasks/ctedilib/tests/TagParserTest.rb
@@ -18,13 +18,13 @@ class TagParserTest < Test::Unit::TestCase
     importer.import()
   end
   
-  def test_800_large
-    test_file_path = File.dirname(__FILE__) + "/../../../../data/cedict/tags/800+800020100915-adv.csv"
+  def test_800_advance_words
+    test_configuration_file = File.dirname(__FILE__) + "/../../../../config/tags_config/file_800_config.yml"
+    configuration = TagsBaseConfiguration.new(test_configuration_file, "tags_800_advance")
+    
+    test_file_path = File.dirname(__FILE__) + configuration.file_name
     parser = CSVParser.new(test_file_path)
     results = parser.run()
-    
-    test_configuration_file = File.dirname(__FILE__) + "/../../../../config/tags_config/file_800_config.yml"
-    configuration = TagsBaseConfiguration.new(test_configuration_file)
     
     importer = Tags800WordsImporter.new(results, configuration)
     importer.import()
