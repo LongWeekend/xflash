ae61d6df72fe43de99ea182465f0924ec95c7d18
rpranata <rendy.pranata@gmail.com>
2011-09-27 12:11:27 +1000

    Just renamed the test name.

--- importer/lib/tasks/ctedilib/tests/CFlashStudySetsImporterTest.rb
+++ importer/lib/tasks/ctedilib/tests/CFlashStudySetsImporterTest.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 
 class CFlashStudySetsImporterTest < Test::Unit::TestCase
   
-  def test_test   
+  def test_import_groups_and_tags  
      importer = GroupImporter.new("cflash_group_config.yml")
      importer.run()
   end
