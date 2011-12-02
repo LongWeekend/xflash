c42648e013e0f47babe472d8d6424d42ec3858ad
Mark Makdad <makdad@gmail.com>
2011-12-02 20:37:50 +0900

    Whoops, wrong assert name

--- jFlash/Tests/PluginTest.m
+++ jFlash/Tests/PluginTest.m
@@ -16,11 +16,11 @@
 
 - (void) testInitFromHash
 {
-  STAssertEqualsObjects(@"Mark's Cards",self.plugin.name);
-  STAssertEqualsObjects(@"FTS-DB",self.plugin.pluginId);
-  STAssertEqualsObjects(@"database",self.plugin.pluginType);
-  STAssertEqualsObjects(@"1.0",self.plugin.version);
-  STAssertEqualsObjects(@"cFlash-FTS-1.0.db",self.plugin.filePath);
+  STAssertEqualObjects(@"Mark's Cards",self.plugin.name);
+  STAssertEqualObjects(@"FTS-DB",self.plugin.pluginId);
+  STAssertEqualObjects(@"database",self.plugin.pluginType);
+  STAssertEqualObjects(@"1.0",self.plugin.version);
+  STAssertEqualObjects(@"cFlash-FTS-1.0.db",self.plugin.filePath);
 }
 
 - (void) testFullPaths
@@ -29,23 +29,50 @@
   
   // Documents
   expectedPath = [LWEFile createDocumentPathWithFilename:self.plugin.filePath];
-  STAssertEqualsObjects(expectedPath, self.plugin.fullPath);
+  STAssertEqualObjects(expectedPath, self.plugin.fullPath);
   
   // Library
   self.plugin.fileLocation = LWEPluginLocationLibrary;
   expectedPath = [LWEFile createLibraryPathWithFilename:self.plugin.filePath];
-  STAssertEqualsObjects(expectedPath, self.plugin.fullPath);
+  STAssertEqualObjects(expectedPath, self.plugin.fullPath);
   
   // Bundle
   self.plugin.fileLocation = LWEPluginLocationBundle;
   expectedPath = [LWEFile createBundlePathWithFilename:self.plugin.filePath];
-  STAssertEqualsObjects(expectedPath, self.plugin.fullPath);
+  STAssertEqualObjects(expectedPath, self.plugin.fullPath);
   
   // Unknown value defaults to bundle
   self.plugin.fileLocation = 6543;
   expectedPath = [LWEFile createBundlePathWithFilename:self.plugin.filePath];
-  STAssertEqualsObjects(expectedPath, self.plugin.fullPath);
+  STAssertEqualObjects(expectedPath, self.plugin.fullPath);
+}
+
+- (void) testVersionDetection
+{
+  NSDictionary *newDict = [NSDictionary dictionaryWithObjectsAndKeys:
+                        @"Mark's FTS Cards 1.1",@"name",
+                        @"FTS-DB",@"pluginId",
+                        @"database",@"pluginType",
+                        @"1.1",@"version",
+                        [NSNumber numberWithInt:LWEPluginLocationDocuments],@"fileLocation",
+                        @"cFlash-FTS-1.1.db",@"filePath",nil];
   
+  NSDictionary *evenNewerDict = [NSDictionary dictionaryWithObjectsAndKeys:
+                        @"Mark's FTS Cards 1.1",@"name",
+                        @"FTS-DB",@"pluginId",
+                        @"database",@"pluginType",
+                        @"1.2.1",@"version",
+                        [NSNumber numberWithInt:LWEPluginLocationDocuments],@"fileLocation",
+                        @"cFlash-FTS-1.2.1.db",@"filePath",nil];
+
+  Plugin *newPlugin = [Plugin pluginWithDictionary:newDict];
+  Plugin *evenNewerPlugin = [Plugin pluginWithDictionary:evenNewerDict];
+  
+  STAssertEqual(YES,[newPlugin isNewVersionOfPlugin:self.plugin]);
+  STAssertEqual(YES,[evenNewerPlugin isNewVersionOfPlugin:self.plugin]);
+  STAssertEqual(YES,[evenNewerPlugin isNewVersionOfPlugin:newPlugin]);
+  STAssertEqual(NO,[self.plugin isNewVersionOfPlugin:newPlugin]);
+  STAssertEqual(NO,[self.plugin isNewVersionOfPlugin:evenNewerPlugin]);
 }
 
 #pragma mark - Setup/Teardown
