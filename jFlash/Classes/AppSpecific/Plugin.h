6f8fab059437427740b93adbd6b0c166f4b867e1
Mark Makdad <makdad@gmail.com>
2011-12-02 18:53:26 +0900

    Initial commit of plugin class

---
+++ jFlash/Classes/AppSpecific/Plugin.h
@@ -0,0 +1,32 @@
+//
+//  Plugin.h
+//  jFlash
+//
+//  Created by Mark Makdad on 12/2/11.
+//  Copyright (c) 2011 Long Weekend LLC. All rights reserved.
+//
+
+
+typedef enum NSInteger
+{
+  LWEPluginLocationBundle = 0,
+  LWEPluginLocationLibrary = 1,
+  LWEPluginLocationDocuments = 2
+} LWEPluginLocation;
+
+@interface Plugin : NSObject <NSCoding>
+
+//! For creating a plugin file out of an NSDictionary hash of strings
++ (id) pluginWithDictionary:(NSDictionary *)dict;
+
+- (BOOL) isDirectoryPlugin;
+- (BOOL) isDatabasePlugin;
+
+@property LWEPluginLocation fileLocation;
+@property (retain) NSString *filePath;
+@property (retain) NSString *name;
+@property (retain) NSString *version;
+@property (retain) NSString *pluginType;
+@property (retain) NSString *pluginId;
+
+@end
---
+++ jFlash/Classes/AppSpecific/Plugin.m
@@ -0,0 +1,94 @@
+//
+//  Plugin.m
+//  jFlash
+//
+//  Created by Mark Makdad on 12/2/11.
+//  Copyright (c) 2011 Long Weekend LLC. All rights reserved.
+//
+
+#import "Plugin.h"
+
+@implementation Plugin
+
+@synthesize name, filePath, pluginId, pluginType, version, fileLocation;
+
+#pragma mark - Initialization/Constructors
+
++ (id) pluginWithDictionary:(NSDictionary *)dict
+{
+  id plugin = [[[[self class] alloc] init] autorelease];
+  for (NSString *key in dict)
+  {
+    // KVC-style setters
+    id value = [dict valueForKey:key];
+    [plugin setValue:value forKey:key];
+  }
+  return plugin;
+}
+
+#pragma mark - NSCoding Support
+
+- (void)encodeWithCoder:(NSCoder *)encoder
+{
+  // NSObject doesn't conform to NSCoding so we don't have to call super
+  [encoder encodeObject:self.name forKey:@"name"];
+  [encoder encodeObject:self.version forKey:@"version"];
+  [encoder encodeObject:self.filePath forKey:@"filePath"];
+  [encoder encodeObject:self.pluginId forKey:@"pluginId"];
+  [encoder encodeObject:self.pluginType forKey:@"pluginType"];
+  [encoder encodeInteger:self.fileLocation forKey:@"fileLocation"];
+}
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+  // NSObject doesn't conform to NSCoding so we don't have to call super
+  self = [super init];
+  if (self)
+  {
+    self.name = [aDecoder decodeObjectForKey:@"name"];
+    self.version = [aDecoder decodeObjectForKey:@"version"];
+    self.filePath = [aDecoder decodeObjectForKey:@"filePath"];
+    self.pluginId = [aDecoder decodeObjectForKey:@"pluginId"];
+    self.pluginType = [aDecoder decodeObjectForKey:@"pluginType"];
+    self.fileLocation = [aDecoder decodeIntegerForKey:@"fileLocation"];
+  }
+  return self;
+}
+
+#pragma mark - Public Methods
+
+- (BOOL) isDirectoryPlugin
+{
+  return YES;
+}
+
+- (BOOL) isDatabasePlugin
+{
+  return YES;
+}
+
+- (BOOL) isEqual:(id)object
+{
+  BOOL returnVal = NO;
+  if ([object isKindOfClass:[Plugin class]])
+  {
+    BOOL sameId = ([self.pluginId isEqualToString:[(Plugin*)object pluginId]]);
+    //    BOOL sameVersion = ([self.version isEqualToString:[(Plugin*)object version]]);
+    returnVal = sameId; //&& sameVersion;
+  }
+  return returnVal;
+}
+
+#pragma mark - Class Plumbing
+
+- (void) dealloc
+{
+  [name release];
+  [version release];
+  [filePath release];
+  [pluginType release];
+  [pluginId release];
+  [super dealloc];
+}
+
+@end
