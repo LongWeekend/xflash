bc33d49ec15a128d03eac6417244b985702c97d6
Mark Makdad <makdad@gmail.com>
2011-12-06 18:30:41 +0900

    Made some failing tests that show how I think Search is funky

--- jFlash/Tests/cFlashTests/CFlashSearchTest.m
+++ jFlash/Tests/cFlashTests/CFlashSearchTest.m
@@ -15,6 +15,13 @@
 
 @implementation CFlashSearchTest
 
+- (NSArray *) _searchKeywordExpectingResults:(NSString*)keyword
+{
+  NSArray *results = [CardPeer fullTextSearchForKeyword:keyword];
+  STAssertTrue(([results count] > 0), @"FTS search returned no results for keyword: %@!", keyword);
+  return results;
+}
+
 #pragma mark - Search Test Methods
 
 - (void) testBasicSearch
@@ -22,6 +29,35 @@
   NSArray *results = [CardPeer fullTextSearchForKeyword:@"keyword"];
   NSInteger resultCount = [results count];
   STAssertEquals(3, resultCount, @"CFlash database should return 3 results for search keyword 'keyword'");
+}
+
+// When I search for "gong1", I would expect to see results whose pinyin *starts* with gong1 first.
+- (void) testMatchPinyinFirst
+{
+  // The first card's reading should match
+  NSArray *results = [self _searchKeywordExpectingResults:@"gong1"];
+  Card *resultCard = [results objectAtIndex:0];
+  [resultCard hydrate];
+  STAssertEqualObjects(@"gong1", resultCard.reading, @"First search results should match pinyin search.");
+}
+
+// When I search for "人", the first match should be only that
+- (void) testMatchExactCharacterFirst
+{
+  // The first card's headword should match
+  NSArray *results = [self _searchKeywordExpectingResults:@"人"];
+  Card *resultCard = [results objectAtIndex:0];
+  [resultCard hydrate];
+  STAssertEqualObjects(@"人", resultCard.headword, @"HW of these cards should be the same");
+}
+
+- (void) testMatchMultiplePinyin
+{
+  NSArray *results = [self _searchKeywordExpectingResults:@"duo1 gong1"];
+  Card *resultCard = [results objectAtIndex:0];
+  [resultCard hydrate];
+  
+  STAssertEqualObjects(@"duo1 gong1", resultCard.reading, @"First search results should match pinyin search.");
 }
 
 #pragma mark - Setup & Teardown
