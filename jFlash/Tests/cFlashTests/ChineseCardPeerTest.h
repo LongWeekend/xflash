07611599ed374199a49eff4da094ade038a273d8
Mark Makdad <makdad@gmail.com>
2011-12-07 23:37:48 +0900

    Added keyword distinction tests

---
+++ jFlash/Tests/cFlashTests/ChineseCardPeerTest.h
@@ -0,0 +1,16 @@
+//
+//  ChineseCardPeerTest.h
+//  jFlash
+//
+//  Created by Mark Makdad on 12/7/11.
+//  Copyright (c) 2011 Long Weekend LLC. All rights reserved.
+//
+
+//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ChineseCardPeerTest : SenTestCase
+
+@end
