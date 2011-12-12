40558de1fdebcae41cfe58e02974c10123aa77a2
Mark Makdad <makdad@gmail.com>
2011-12-12 16:25:26 +0900

    This CG-fu goes out to my homie Paul

---
+++ jFlash/Classes/AppSpecific/CloseButtonView.h
@@ -0,0 +1,13 @@
+//
+//  CloseButtonView.h
+//  jFlash
+//
+//  Created by Mark Makdad on 12/12/11.
+//  Copyright (c) 2011 Long Weekend LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CloseButtonView : UIView
+
+@end
