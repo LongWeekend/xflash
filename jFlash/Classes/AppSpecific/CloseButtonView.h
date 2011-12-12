925eea348e66a4f4617a437b26008a5bd99c53be
Mark Makdad <makdad@gmail.com>
2011-12-12 16:28:44 +0900

    Moved button config vars to header, improved positioning & size in the XIB, made it look slightly Awesomer

--- jFlash/Classes/AppSpecific/CloseButtonView.h
+++ jFlash/Classes/AppSpecific/CloseButtonView.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+// The width in points of the border around the view
+#define CLOSE_BUTTON_VIEW_BORDER_WIDTH 1.5f
+
+// The % (between 0-1, where 1 = 100%) of the view that is a margin around the X at the center
+// This value seems to be very sensitive between .25-.4
+#define CLOSE_BUTTON_VIEW_X_SIZE 0.315f
+
+// A CGSize saying how far the shadow should go
+#define CLOSE_BUTTON_SHADOW_OFFSET CGSizeMake(2.0f, 2.0f)
+
+// Blur value for the shadow.
+#define CLOSE_BUTTON_SHADOW_BLUR 0.5f
+
+// Stroke width of the "X"
+#define CLOSE_BUTTON_X_WIDTH 3.0f
+
 @interface CloseButtonView : UIView
 
 @end
--- jFlash/Classes/AppSpecific/CloseButtonView.m
+++ jFlash/Classes/AppSpecific/CloseButtonView.m
@@ -16,22 +16,6 @@
 @implementation CloseButtonView
 @synthesize isSelected;
 
-// The width in points of the border around the view
-#define CLOSE_BUTTON_VIEW_BORDER_WIDTH 1.5f
-
-// The % (between 0-1, where 1 = 100%) of the view that is a margin around the X at the center
-#define CLOSE_BUTTON_VIEW_X_SIZE 0.3f
-
-// A CGSize saying how far the shadow should go
-#define CLOSE_BUTTON_SHADOW_OFFSET CGSizeMake(2.0f, 2.0f)
-
-// Blur value for the shadow.
-#define CLOSE_BUTTON_SHADOW_BLUR 0.5f
-
-#define CLOSE_BUTTON_X_WIDTH 3.0f
-
-#pragma mark - Touch Code
-
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
   // Set a bit so we re-draw the button in a selected state
