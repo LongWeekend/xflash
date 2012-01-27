d37f703faa0487cb55100adc925900e624abb9c6
todd <todd@TheNewOne>
2012-01-26 20:10:04 -0800

    added basic TabHost setup

--- android/src/com/longweekend/android/jflash/Jflash.java
+++ android/src/com/longweekend/android/jflash/Jflash.java
@@ -6,7 +6,9 @@ package com.longweekend.android.jflash;
 //  Created by Todd Presson on 1/7/12.
 //  Copyright 2012 LONG WEEKEND INC.. All rights reserved.
 
-import android.app.Activity;
+import android.content.res.Resources;
+import android.widget.TabHost;
+import android.app.TabActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -22,31 +24,11 @@ import android.widget.Button;
 import com.longweekend.android.jflash.model.LWEDatabase;
 import com.longweekend.android.jflash.model.Card;
 
-public class Jflash extends Activity
+public class Jflash extends TabActivity
 {
     private static final String MYTAG = "JFlash main";
     
-    private DBReceiver myReceiver;
-
-    // boolean for checking whether database is already open
-    private boolean firedUp;
-
-    // also for debugging
-    private LinearLayout myLayout;
-
-    public void pop1(View v) {
-
-        Card myCard = new Card();
-        myCard.setCardId(12204);
-        myCard.hydrate();
-
-        // new commit
-        TextView tempView = (TextView)findViewById(R.id.tempview);
-        tempView.setText( myCard.meaningWithoutMarkup() );
-    }
-
     
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -54,138 +36,22 @@ public class Jflash extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        firedUp = false;
-        myReceiver = null;
-
-        // asdf
-        myLayout = (LinearLayout)findViewById(R.id.mainlayout);
-    }
-
-
-    // this is a bad place to take care of this, just temporary for debugging
-    public void onResume()
-    {
-        super.onResume();
+        // resource object to get drawables
+        Resources res = getResources();     
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec;
         
-        // if our receiver isn't running, start it
-        if( myReceiver == null )
-        {
-            IntentFilter intentFilter;
-            myReceiver = new DBReceiver();
+        // reusable intent for each tab
+        Intent intent;
 
-            intentFilter = new IntentFilter(com.longweekend.android.jflash.model.LWEDatabase.COPY_START);
-            registerReceiver(myReceiver,intentFilter);
-            intentFilter = new IntentFilter(com.longweekend.android.jflash.model.LWEDatabase.COPY_START2);
-            registerReceiver(myReceiver,intentFilter);
-            intentFilter = new IntentFilter(com.longweekend.android.jflash.model.LWEDatabase.COPY_SUCCESS);
-            registerReceiver(myReceiver,intentFilter);
-            intentFilter = new IntentFilter(com.longweekend.android.jflash.model.LWEDatabase.COPY_FAILURE);
-            registerReceiver(myReceiver,intentFilter);
-            intentFilter = new IntentFilter(com.longweekend.android.jflash.model.LWEDatabase.DATABASE_READY);
-            registerReceiver(myReceiver,intentFilter);
-        }
-        
-        // if our database is not set up, get it started and open it
-        if( !firedUp )
-        {
-            // set our master database instance
-            LWEDatabase tempDB = JFApplication.getDao();
-            
-            // check if our databases have been copied,
-            // copy them if they haven't
-            tempDB.asynchCopyDatabaseFromAPK();
-        }
-    
-    }  // end onResume()
+        intent = new Intent().setClass(this,PracticeActivity.class);
 
+        spec = tabHost.newTabSpec("practice").setIndicator("Practice",
+                        res.getDrawable(R.drawable.target_flip)).setContent(intent);
+        tabHost.addTab(spec);
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
+        tabHost.setCurrentTab(0);
 
-        if( myReceiver != null )
-        {
-            unregisterReceiver(myReceiver);
-            myReceiver = null;
-        }
     }
 
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-
-        LWEDatabase tempDB = JFApplication.getDao();
-        tempDB.detachDatabase();
-        tempDB.closeDatabase();
-    }
-
-    
-    // our receiver class for broadcasts sent from the database 
-    // TODO - temporary for debugging purposes
-    protected class DBReceiver extends BroadcastReceiver
-    {
-        @Override
-        public void onReceive(Context context,Intent intent)
-        {
-            TextView tempView = new TextView(context);
-
-            // in this case, update the song list when it advances (on shuffle)
-            if( intent.getAction().equals(com.longweekend.android.jflash.model.LWEDatabase.COPY_START))
-            {
-                tempView.setText("Copying db 1...");
-                myLayout.addView(tempView);
-            }
-            else if( intent.getAction().equals(com.longweekend.android.jflash.model.LWEDatabase.COPY_START2))
-            {
-                tempView.setText("Copying db 2...");                                
-                myLayout.addView(tempView);
-            }
-            else if( intent.getAction().equals(com.longweekend.android.jflash.model.LWEDatabase.COPY_SUCCESS))
-            {
-                tempView.setText("Copy Success");   
-                myLayout.addView(tempView);
-            }
-            else if( intent.getAction().equals(com.longweekend.android.jflash.model.LWEDatabase.COPY_FAILURE))
-            {
-                tempView.setText("Copy Failure");                                
-                myLayout.addView(tempView);
-            }
-            else if( intent.getAction().equals(com.longweekend.android.jflash.model.LWEDatabase.DATABASE_READY))
-            {
-                try
-                {
-                    LWEDatabase tempDB = JFApplication.getDao();
-                    firedUp = tempDB.attachDatabase();
-                    
-                    if( firedUp )
-                    {
-                        Button tempButton = (Button)findViewById(R.id.testbutton);
-                        tempButton.setVisibility(View.VISIBLE); 
-    
-                        tempView.setText("Database attached");
-                        myLayout.addView(tempView);
-                    }
-                    else
-                    {
-                        tempView.setText("Database attach failed");
-                        myLayout.addView(tempView);
-                    } 
-                }
-                catch (Exception e)
-                {
-                    Log.d(MYTAG,"Exception caught attaching DB:  " + e.toString() );
-                }
-            }
-
-        }  // end onReceive()
-
-    }  // end DBReceiver declaration
-
-
-
-}  // end Jflash class declaration
-
-
+}
