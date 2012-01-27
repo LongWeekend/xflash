ecd03d4ff7cbdde43f95acefac23b2eba1703682
todd <todd@TheNewOne>
2012-01-27 13:25:43 -0800

    added basic HelpActivity layout

--- android/src/com/longweekend/android/jflash/HelpActivity.java
+++ android/src/com/longweekend/android/jflash/HelpActivity.java
@@ -6,13 +6,16 @@ package com.longweekend.android.jflash;
 //  Created by Todd Presson on 1/26/2012.
 //  Copyright 2012 LONG WEEKEND INC.. All rights reserved.
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class HelpActivity extends Activity
+public class HelpActivity extends ListActivity
 {
     private static final String MYTAG = "JFlash HelpActivity";
     
+    // asdf
 
     /** Called when the activity is first created. */
     @Override
@@ -20,6 +23,12 @@ public class HelpActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+
+        Resources res = getResources();
+        String[] topics = res.getStringArray(R.array.help_topics);
+        setListAdapter(new ArrayAdapter<String>(this,R.layout.help_row,
+                                                R.id.help_label,topics));
+
     }
 
 
