e9a4b7eb87b400284444258610ccfa2e1fab7355
Mark Makdad <makdad@gmail.com>
2012-05-06 18:20:37 +0900

    Removed misnamed migration file

--- jFlash/Migrations/cflash_102_to_11.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users_new (user_id INTEGER PRIMARY KEY, nickname TEXT);
-INSERT INTO users_new SELECT user_id, nickname FROM users;
-DROP TABLE users;
-ALTER TABLE users_new RENAME TO users;
