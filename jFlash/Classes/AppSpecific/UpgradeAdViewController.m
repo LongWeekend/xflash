856fab2e927d8ea505e5df1882aeb23d3b5a6355
Mark Makdad <makdad@gmail.com>
2012-06-07 01:17:13 -0400

    Added switch to support both C & JFlash

--- jFlash/Classes/AppSpecific/UpgradeAdViewController.m
+++ jFlash/Classes/AppSpecific/UpgradeAdViewController.m
@@ -20,7 +20,13 @@
 - (IBAction)appStoreBtnPressed:(id)sender
 {
   LWENetworkUtils *tmpNet = [[LWENetworkUtils alloc] init];
+#if defined (LWE_JFLASH)
+  // TODO: Replace this with a JFlash link
   [tmpNet followLinkshareURL:@"http://click.linksynergy.com/fs-bin/stat?id=qGx1VSppku4&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=http%253A%252F%252Fitunes.apple.com%252Fus%252Fapp%252Fid380853144%253Fmt%253D8%2526uo%253D4%2526partnerId%253D30&u1=JFLASH_APP_WELCOME_MESSAGE"];
+#elif defined (LWE_CFLASH)
+  // TODO: Replace this with a CFlash link
+  [tmpNet followLinkshareURL:@"http://click.linksynergy.com/fs-bin/stat?id=qGx1VSppku4&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=http%253A%252F%252Fitunes.apple.com%252Fus%252Fapp%252Fid380853144%253Fmt%253D8%2526uo%253D4%2526partnerId%253D30&u1=JFLASH_APP_WELCOME_MESSAGE"];
+#endif
   [tmpNet release];
 }
 
