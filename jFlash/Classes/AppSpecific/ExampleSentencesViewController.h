433689f796e9ca30d73ea62e7de2536ecd618a50
Mark Makdad <makdad@gmail.com>
2012-06-07 01:57:18 -0400

    Added a few more comments

--- jFlash/Classes/AppSpecific/ExampleSentencesViewController.h
+++ jFlash/Classes/AppSpecific/ExampleSentencesViewController.h
@@ -10,9 +10,13 @@
 #import "Card.h"
 #import "Plugin.h"
 
+// These are strings that will be used in the UIWebView's links, then as delegate we 
+// intercept those links and determine what "button" the user tapped.  Similar to a UIView
+// tag in the way we use it.
 extern NSString * const TOKENIZE_SAMPLE_SENTENCE;
 extern NSString * const ADD_CARD_TO_SET;
 
+// The HTML template we show the example sentences in.
 extern NSString * const LWESentencesHTML;
 
 @interface ExampleSentencesViewController : UIViewController <UIWebViewDelegate>
